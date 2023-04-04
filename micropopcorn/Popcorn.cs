454640422051e9f9ac91f870d4c25627e9fb93f7
rieerep <114030611+rieerep@users.noreply.github.com>
2023-04-04 22:52:36 +0200

    first commit

---
+++ micropopcorn/Popcorn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace micropopcorn
+{
+    internal class Popcorn
+    {
+        public int popId { get; set; }
+        public string Name { get; set; }
+        public decimal PopPercentage { get; set; }
+
+    }
+}
---
+++ micropopcorn/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace micropopcorn
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Popcorn butterPop = new Popcorn
+            {
+                popId = 1,
+                Name = "Smörpopcorn",
+                PopPercentage = 0
+            };
+            Console.WriteLine(butterPop.Name);
+            var butterPopTask = await Microwaving(butterPop);
+            Console.WriteLine(butterPopTask);
+        }
+
+        public async static Task<Popcorn> Microwaving(Popcorn popcorn)
+        {
+            int tick = 30;
+            while (true)
+            {
+                await Wait(tick);
+                popcorn.PopPercentage += (0.55M * tick);
+
+                if (popcorn.PopPercentage >= 99)
+                {
+                    Console.WriteLine("Pop done");
+                    return popcorn;
+                }
+            }
+            Console.WriteLine("Startade microwaving");
+            await Wait();
+            await Console.Out.WriteLineAsync("3 minuters micropop klar.");
+            return popcorn;
+        }
+        private static async Task Wait(double delay = 1)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(delay));
+            Console.WriteLine("30 seconds wait completed.");
+        }
+    }
+
+    public async static Task popcornStatus(Popcorn popcorn)
+    {
+
+    }
+}
